3083b843d31cffef5d6d48580866cb4cfb47f27b
gyu0025 <rbdnjs524@naver.com>
2020-05-13 13:20:13 +0900

    pipline yaml edit

--- src/main/java/roomreservation/Reservation.java
+++ src/main/java/roomreservation/Reservation.java
@@ -20,11 +20,7 @@ public class Reservation {
         Reserved reserved = new Reserved();
         BeanUtils.copyProperties(this, reserved);
         reserved.publish();
-
-
     }
-
-
     @PostRemove
     public void onPostRemove(){
         ReservationCanceled reservationCanceled = new ReservationCanceled();
@@ -36,33 +32,26 @@ public class Reservation {
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
     public String getUserId() {
         return userId;
     }
-
     public void setUserId(String userId) {
         this.userId = userId;
     }
     public String getReserveId() {
         return reserveId;
     }
-
     public void setReserveId(String reserveId) {
         this.reserveId = reserveId;
     }
     public String getStatus() {
         return status;
     }
-
     public void setStatus(String status) {
         this.status = status;
     }
-
-
-
 
 }
